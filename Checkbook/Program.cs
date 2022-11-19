6b656c6caed70ae589b6bfd3a2de93c67aa93c53
Ross Ylitalo <ross.ylitalo@gmail.com>
2022-11-19 05:18:54 -0500

    Add project files.

---
+++ Checkbook/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkbook
+{
+    internal class Program
+    {
+        static cCheckbook _book;
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                _book = new cCheckbook(args[0]);
+            }
+            else
+            {
+                Console.Write("Enter staring Balance: ");
+                _book = new cCheckbook(Console.ReadLine());
+            }
+            Console.WriteLine($"Starting new Checkbook with a balance of {_book}.");
+            while (_book.WritingChecks)
+            {
+                Console.Write("Enter Check Amount: ");
+                decimal amount;
+                while (!decimal.TryParse(Console.ReadLine(), out amount))
+                {
+                    Console.WriteLine("Illegal Amount.");
+                    Console.Write("Enter Check Amount: ");
+                }
+                _book.WriteCheck(amount);
+                _book.WriteBalance();
+            }
+        }
+    }
+}
---
+++ Checkbook/cCheckbook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkbook
+{
+    internal class cCheckbook
+    {
+        decimal _balance = 0;
+        public bool WritingChecks { get; internal set; } = true;
+        public cCheckbook(string amount)
+        {
+            _balance = decimal.Parse(amount);
+        }
+
+        public decimal WriteCheck(string amount)
+        {
+            decimal checkAmount = decimal.Parse(amount);
+            _balance -= checkAmount;
+            return _balance;
+        }
+
+        internal decimal WriteCheck(decimal amount)
+        {
+            _balance -= amount;
+            return _balance;
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+        public void WriteBalance()
+        {
+            Console.WriteLine(FormattedBalance);
+        }
+        public string FormattedBalance
+        {
+            get { return string.Format("{0:C}", _balance); }
+        }
+
+        public override string ToString()
+        {
+            return FormattedBalance;
+        }
+    }
+}
